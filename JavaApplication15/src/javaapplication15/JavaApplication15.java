3d66a524b6c423e0de1f190b38d42be1bc3423a8
fxalban8 <francisco.alban@epn.edu.ec>
2020-06-15 12:27:07 -0500

    Insertion Method implemented

---
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication15;
+
+/**
+ *
+ * @author dell
+ */
+public class JavaApplication15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void ordenarArregloPorInsercion(int [] arreglo){
+
+        int pos, aux;
+        
+        for (int i=0; i<arreglo.length;i++){   
+            pos=i;  //guarda la posicion
+            aux=arreglo[i]; //guarda el número de esa posicion
+            
+            while((pos>0)&&arreglo[pos-1]>aux){
+                arreglo[pos]=arreglo[pos-1];
+                pos--;
+            }
+            arreglo[pos]=aux;
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int array1[] = {29,34,23,45,6};
+        
+        ordenarArregloPorInsercion(array1);
+
+        for(int i: array1){
+            System.out.print(i+" ");
+        }
+    }
+}
