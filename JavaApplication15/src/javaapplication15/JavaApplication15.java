4c55fd0ce4439b3f51cb02aab64b542fbd45a7a1
fxalban8 <francisco.alban@epn.edu.ec>
2020-06-15 12:54:31 -0500

    Giving new values

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -15,9 +15,7 @@ public class JavaApplication15 {
      * @param args the command line arguments
      */
     public static void ordenarArregloPorInsercion(int [] arreglo){
-
-        int pos, aux;
-        
+        int pos, aux;    
         for (int i=0; i<arreglo.length;i++){   
             pos=i;  //guarda la posicion
             aux=arreglo[i]; //guarda el número de esa posicion
@@ -33,7 +31,7 @@ public class JavaApplication15 {
     public static void main(String[] args) {
         // TODO code application logic here
         double start_time= System.nanoTime();
-        int array1[] = {29,34,23,45,6};
+        int array1[] = {3,2,3,4,8,29,34,12,24,56,78,12,21,6,3,63,56,99,145,23,12,89,23,45,23,14,15,23,45,78,99,123,18,3,6,23,46,23,12,67,43,56,78,1,5,4,6,99,12,8,67,43,21,89,123,56,21,56,12,123,56,23,34,56,12,12,1,89,23,56,12,85,89,12,34,56,23,45,97,67,24,56,12,5,8,23,45,11,72,49,109,185,122,2,2,23,341,237,112,109};
         
         ordenarArregloPorInsercion(array1);
 
